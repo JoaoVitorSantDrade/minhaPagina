2ac2a1f7700f37aae452b3f2d0656503e44c1862
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-17 22:12:45 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.postcss';
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+
+<ol class="breadcrumb">
+	<li class="crumb"><a class="anchor" href="/about">Sobre mim</a></li>
+</ol>
+
+<slot />
