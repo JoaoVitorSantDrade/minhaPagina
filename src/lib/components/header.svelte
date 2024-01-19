66d069793383b54f0cb8f5f78af17db45f9022b9
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-19 12:01:56 -0300

    Remove UK flag SVG and update flag images in header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { getContext } from 'svelte';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
-
+	import brazilFlag from '$lib/assets/flags/brazil.svg';
+	import usFlag from '$lib/assets/flags/us.svg';
 	const i18n = getContext('i18n');
 </script>
 
@@ -12,13 +13,13 @@
 			aria-label={$i18n.t('navbar.flags.brazil')}
 			on:click={() => $i18n.changeLanguage('pt_br')}
 		>
-			<img src="./src/lib/assets/flags/brazil.svg" height="20px" width="30px" alt="Brasil" />
+			<img src={brazilFlag} height="20px" width="30px" alt="Brasil" />
 		</button>
 		<button
 			class="btn px-4 h-10 hover:variant-outline"
 			aria-label={$i18n.t('navbar.flags.us')}
 			on:click={() => $i18n.changeLanguage('en')}
-			><img src="./src/lib/assets/flags/us.svg" height="20px" width="30px" alt="United States" />
+			><img src={usFlag} height="20px" width="30px" alt="United States" />
 		</button>
 	</div>
 	<div class="flex flex-row p-4 gap-8 items-center">
