c14dbc1fd5b3ae0aa849a2a54e5b90fffd90054c
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-20 03:47:14 -0300

    Update database connection and query clients

--- src/lib/db/conn.server.ts
+++ src/lib/db/conn.server.ts
@@ -1,7 +1,6 @@
-import { drizzle as old_drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-
-// import 'dotenv/config';
+import 'dotenv/config';
 
 const conString = process.env.POSTGRES_URL!;
 
@@ -9,17 +8,7 @@ export const migrationConnection = postgres(conString, {
 	max: 1
 });
 
-// export const queryClient = postgres(conString, {
-// 	host: process.env.POSTGRES_HOST,
-// 	database: process.env.POSTGRES_DATABASE,
-// 	password: process.env.POSTGRES_PASSWORD,
-// 	user: process.env.POSTGRES_USER
-// });
+export const queryClient = postgres(conString);
 
-// export const conn = drizzle(queryClient);
-export const migrationClient = old_drizzle(migrationConnection);
-
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
-
-export const conn = drizzle(sql);
+export const conn = drizzle(queryClient);
+export const migrationClient = drizzle(migrationConnection);
