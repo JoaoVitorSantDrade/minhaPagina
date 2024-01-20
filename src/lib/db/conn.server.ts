c1e6bab5cd4f5690c6c66103d725e40991110c06
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-20 02:39:26 -0300

    Refactor code to improve performance and readability

--- src/lib/db/conn.server.ts
+++ src/lib/db/conn.server.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+
 import 'dotenv/config';
 
 const conString = process.env.POSTGRES_URL!;
@@ -8,7 +9,12 @@ export const migrationConnection = postgres(conString, {
 	max: 1
 });
 
-const queryClient = postgres(conString);
+export const queryClient = postgres(conString, {
+	host: process.env.POSTGRES_HOST,
+	database: process.env.POSTGRES_DATABASE,
+	password: process.env.POSTGRES_PASSWORD,
+	user: process.env.POSTGRES_USER
+});
 
-export const migrationClient = drizzle(migrationConnection);
 export const conn = drizzle(queryClient);
+export const migrationClient = drizzle(migrationConnection);
