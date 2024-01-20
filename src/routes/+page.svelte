9a03b50df09e84335e021a925f48114402b09b89
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-20 05:13:28 -0300

    Update package.json and +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,10 @@
 <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-16 py-2">
 	{#await data.projects}
 		{#each Array(2) as _}
-			{import.meta.env.MSG}
 			<PlaceholderCard />
 		{/each}
 	{:then projects}
 		{#each Array(Math.min(3, projects.length)) as _, i}
-			{import.meta.env.VITE_MSG}
 			<ProjectCard projeto={projects[i].project} tech={projects[i].technologies}></ProjectCard>
 		{/each}
 	{/await}
